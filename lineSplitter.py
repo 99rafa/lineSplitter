adb58c67289f0752dbe7cd0ea8089154bc2f97b9
Rafael Alexandre <r.alexandre99@gmail.com>
2020-05-09 02:37:59 +0100

    initial commit

---
+++ lineSplitter.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import sys
+
+running = True
+
+while running:
+    print("What is the path of the css file you want to split? (q to exit)")
+    file = input()
+
+    if file == 'q':
+        sys.exit()
+
+
+    my_file = Path(file)
+    output = ""
+
+    if my_file.is_file():
+        f = open(file,"r")
+        for line in f:
+            for char in line:
+                if char == '{' or char == ';':
+                    output += ' ' + char + '\n'
+                elif char == '}':
+                    output += '\n' + char +'\n\n'
+
+                else:
+                    output += char
+        f.close()
+        print("What is the path of the output css file?")
+        file = input()
+        o = open(file,"w")
+        o.write(output)
+        o.close()
+        print("File splitted succesfully!")
+
+    else:
+        print("Error: File does not exist, try again.")
+
+
+
+
+    
